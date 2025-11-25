e93da4d84b79ef571fa150ce41c3371759b93dec
sarahqii <yueran79@gmail.com>
2025-11-24 22:09:54 -0600

    Update awards.ts

--- src/data/awards.ts
+++ src/data/awards.ts
@@ -7,39 +7,15 @@ export interface AwardItem {
 
 export const awards: AwardItem[] = [
   {
-    title: "Graduate Research Fellowship for Master's Student",
-    description: "A national fellowship awarded by the NRF to foster Master's students' early research capabilities and academic excellence",
-    issuer: "NRF",
-    date: "2024 - 2025",
+    title: "Pi515 AI Challenge",
+    description: "Selected as a finalist team in the Pi515 Al Challenge for developing Tech Upskilling, a web application that uses user quiz data and career insights to recommend tech skills and curated learning resources for individuals exploring or transitioning into careers in the insurance industry.",
+    issuer: "Pi515",
+    date: "2025.04",
   },
   {
-    title: "Outstanding TA Award",
-    description: "Teaching assistant for CS206 Data Structure course",
-    issuer: "KAIST",
-    date: "2025.07",
-  },
-  {
-    title: "Outstanding TA Award",
-    description: "Teaching assistant for CS206 Data Structure course",
-    issuer: "KAIST",
-    date: "2024.08",
-  },
-  {
-    title: "Lim Mi-sook Scholarship",
-    description: "A scholarship that supports KAIST School of Computing female students who demonstrate outstanding leadership",
-    issuer: "KAIST",
-    date: "2023.11",
-  },
-  {
-    title: "National Science and Engineering Scholarship",
-    description: "A government-funded program in South Korea for outstanding students entering science and engineering fields",
-    issuer: "KAIST",
-    date: "2018.06",
-  },
-  {
-    title: "KAIST Presidential Fellowship",
-    description: "A selective KAIST program supporting top undergraduates with academic excellence and leadership potential to become global science and technology leaders",
-    issuer: "KAIST",
-    date: "2018 - 2023",
+    title: "Dean's List Fall Semester 2023",
+    description: "Achieved a semester GPA of 3.75 or higher while completing 16 credits, including 14 letter-graded credits.",
+    issuer: "Grinnell",
+    date: "2023.12",
   },
 ]; 
